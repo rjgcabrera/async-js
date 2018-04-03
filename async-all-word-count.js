d7fcff9670edee6dcb3054ad29f0dfcfbe6b9908
Raymond Cabrera <rjgcengr@gmail.com>
2018-04-03 09:46:44 -0700

    Implement countAllWords

--- async-all-word-count.js
+++ async-all-word-count.js
@@ -1,40 +1,40 @@
-/*        
-   ----- unnecessary background story -----    
+/*
+   ----- unnecessary background story -----
 
 Your grade school Math teacher visited you at Hack Reactor, expecting
-you to do an old assignment to see if you're still a smart cookie. 
-In fact, your teacher expects you to do this every week to make sure 
-you keep practiced! How frustrating! 
+you to do an old assignment to see if you're still a smart cookie.
+In fact, your teacher expects you to do this every week to make sure
+you keep practiced! How frustrating!
 
-The assignment: Count the number of distinct words in a paragraph and 
-submit it to her. 
+The assignment: Count the number of distinct words in a paragraph and
+submit it to her.
 
 However, you now have the power of code to do it within an instant and
-can do it in a fraction of a second without her knowing :) 
+can do it in a fraction of a second without her knowing :)
 
------ end of unnecessary background story ----- 
+----- end of unnecessary background story -----
 
-Write a function that reads an input file and outputs a .txt file 
-containing the word count of every word in your input.txt. 
-This is purely to practice use of asynchronous functions! 
+Write a function that reads an input file and outputs a .txt file
+containing the word count of every word in your input.txt.
+This is purely to practice use of asynchronous functions!
 See exampleOutput folder's sampleOutput.txt for what your output should look like
 with the given input.txt
 
-Constraints: 
+Constraints:
 1) No punctuation will be in your paragraph
 2) This is case-insensitive, so apple and Apple both count as the same word
 
-Allowed documentation: 
+Allowed documentation:
 - Node.js documentation for FS library (https://nodejs.org/api/fs.html)
 
 /*                                                  */
 
 const fs = require('fs');
 
-// countWords is a helper function... it helps you convert a paragraph into 
+// countWords is a helper function... it helps you convert a paragraph into
 // the string you need
 
-// EXAMPLE USAGE 
+// EXAMPLE USAGE
 // countWords('hello world Hello me') --> 'hello: 2\nme:1\nworld: 1\n'
 const countWords = function(paragraph) {
   let words = paragraph.trim('.').split(' ');
@@ -42,7 +42,7 @@ const countWords = function(paragraph) {
   let wordBank = {};
 
   for (let i = 0; i < words.length; i++) {
-    let word = words[i].toLowerCase();
+    let word = words[i].replace(/[^A-Za-z0-9_]/g,"").toLowerCase();
     if (wordBank.hasOwnProperty(word)) {
       wordBank[word] += 1;
     } else {
@@ -57,7 +57,7 @@ const countWords = function(paragraph) {
   wordsFound.forEach(word => {
     outputString += `${word}: ${wordBank[word]}\n`;
   })
-  
+
   return outputString;
 }
 
@@ -66,5 +66,25 @@ const countWords = function(paragraph) {
 // EXAMPLE USAGE
 // countAllWords('./input.txt', './output.txt') --> should output a .txt file in same directory
 var countAllWords = function(inputFile, outputFile) {
-  /* WRITE CODE HERE */ 
+  /* WRITE CODE HERE */
+  var fileContent;
+  fs.readFile(inputFile, 'utf8', (err, data) => {
+    if (err) {
+      console.log('ERROR READING FILE: ', err);
+      throw err;
+    } else {
+      fileContent = data;
+      //console.log(fileContent);
+      fs.writeFile(outputFile, countWords(fileContent), (err) => {
+        if (err) {
+          console.log('ERROR WRITING FILE: ', err);
+          throw err;
+        } else {
+          console.log('WRITE FILE SUCCESS :D');
+        }
+      });
+    }
+  });
 }
+
+countAllWords('./input.txt', './output.txt');
